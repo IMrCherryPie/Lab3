ab27cb5df4ced4020a7452b5b85023aeda114351
IMrCherryPie <47880887+IMrCherryPie@users.noreply.github.com>
2019-04-24 19:46:38 +0300

    Add files via upload
    Over lab3

---
+++ laba3/ArrayTest/ArrayTest/Arrs.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+    class Arrs
+    {
+        private static Random rnd = new Random(Guid.NewGuid().ToByteArray().Sum(x => x));
+        public static void CreateRandDimAr(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(10, 50);
+        }
+        public static void PrintArr(int[] arr)
+        {
+            if (arr == null) Console.WriteLine("Empty");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+        public static void CreateRandDimAr(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int y = 0; y < array.GetLength(1); y++)
+                    array[i, y] = rnd.Next(10, 50);
+        }
+        public static void PrintArr(int[,] arr)
+        {
+            if (arr == null) Console.WriteLine("Empty");
+            else
+            {
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    for (int y = 0; y < arr.GetLength(1); y++)
+                        Console.Write(arr[i, y] + " ");
+                    Console.WriteLine();
+                }
+            }
+        }
+        public static int[,] MultMatr(int[,] arr1, int[,] arr2)
+        {
+            if (arr1.GetLength(1) != arr2.GetLength(0)) return null;
+            int[,] Mass = new int[arr1.GetLength(0), arr2.GetLength(1)];
+            int sum = 0;
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr2.GetLength(1); j++)
+                {
+                    for (int r = 0; r < arr2.GetLength(0); r++)
+                    {
+                        sum = sum + arr1[i, r] * arr2[r, j];
+                    }
+                    Mass[i, j] = sum;
+                    sum = 0;
+                }
+            }
+            return Mass;
+        }
+        public static void PrintArr(int[][] arr)
+        {
+            if (arr == null) Console.WriteLine("Empty");
+            foreach (var t in arr)
+            {
+                for (int i = 0; i < t.Length; i++)
+                    Console.Write(t[i] + " ");
+                Console.WriteLine();
+            }
+        }
+        public static void CreateRandDimAr(int[][] array)
+        {
+            if (array != null)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    var k = new int[i + 1];
+                    for (int j = 0; j <= i; j++)
+                        k[j] = rnd.Next(10, 100);
+                    array[i] = k;
+                }
+            }
+        }
+    }
+}
---
+++ laba3/ArrayTest/ArrayTest/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] i= new int[45];
+            // Arrs t = new Arrs();
+            //Arrs.CreateOneDimAr(i);
+            // foreach (var d in i)
+            // {
+            //    Console.Write(" "+d); 
+            // }
+
+            Quest_1();
+            Quest_2();
+            Quest_3();
+            Console.ReadKey();
+        }
+
+        private static void Quest_1()
+        {
+            Console.WriteLine("Задание 1");
+            int[] A = new int[5], B = new int[5], C = new int[5], X = { 5, 5, 6, 6, 7, 7 }, U, V;
+            //присвоение массива
+            U = new int[3]; for (int i = 1; i <= U.Length; i++)
+                U[i - 1] = i;
+            V = new int[] { 1, 2, 3 };
+
+            V = U;
+
+            V[0] = 9;
+            //рандомно заполняем массивы а и б
+            Arrs.CreateRandDimAr(A); Arrs.CreateRandDimAr(B);
+            //находим сумму двух массивов
+            for (int i = 0; i < C.Length; i++)
+                C[i] = A[i] + B[i];
+
+            //выводы
+            Console.Write("Array A:"); Arrs.PrintArr(A);
+            Console.Write("Array B:"); Arrs.PrintArr(B);
+            Console.Write("Array C(summ):"); Arrs.PrintArr(C);
+            Console.Write("Array X:"); Arrs.PrintArr(X);
+            Console.Write("Array U:"); Arrs.PrintArr(U);
+            Console.Write("Array V:"); Arrs.PrintArr(V);
+
+            Console.WriteLine("Введите размерность массива:");
+            int size = int.Parse(Console.ReadLine());
+            int[] D = new int[size];
+            Arrs.CreateRandDimAr(D);
+            Console.Write("Array D:"); Arrs.PrintArr(D);
+        }
+        private static void Quest_2()
+        {
+            Console.WriteLine("\nЗадание 2");
+            int[,] A = new int[2, 3];
+            int[,] B = new int[3, 2];
+            Arrs.CreateRandDimAr(A);
+            Console.WriteLine("Matrix a:"); Arrs.PrintArr(A); Console.WriteLine();
+            Arrs.CreateRandDimAr(B);
+            Console.WriteLine("Matrix b:"); Arrs.PrintArr(B); Console.WriteLine();
+            int[,] C = Arrs.MultMatr(A, B);
+            Console.WriteLine("Multyplied matrix:"); Arrs.PrintArr(C); Console.WriteLine();
+        }
+        private static void Quest_3()
+        {
+            Console.WriteLine("\nЗадание 3");
+            int[][] arr = new int[10][];
+            Arrs.CreateRandDimAr(arr);
+            Arrs.PrintArr(arr);
+        }
+    }
+}
